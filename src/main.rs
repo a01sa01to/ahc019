821aadc63bc418541004d32a57c4503fa8557a4b
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-03-20 13:22:00 +0900

    feat: 山登りっぽく、ランダムにつなげる

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 extern crate rand;
 use rand::Rng;
 use std::{
-    collections::HashSet,
+    collections::{HashMap, HashSet},
     io, mem,
     time::{Duration, Instant},
 };
 
 const INF: usize = 1_000_000_000;
+const DX: [i32; 6] = [1, 0, 0, -1, 0, 0];
+const DY: [i32; 6] = [0, 1, 0, 0, -1, 0];
+const DZ: [i32; 6] = [0, 0, 1, 0, 0, -1];
 
 fn input() -> (
     usize,
@@ -41,7 +44,6 @@ fn input() -> (
 fn output(ans1: Vec<Vec<Vec<usize>>>, ans2: Vec<Vec<Vec<usize>>>) {
     let mut set = ans1.iter().flatten().flatten().collect::<HashSet<_>>();
     set.extend(ans2.iter().flatten().flatten());
-    eprintln!("{:?}", set);
     println!("{}", set.len() - 1);
     for i in 0..ans1.len() {
         for j in 0..ans1.len() {
@@ -172,25 +174,143 @@ fn main() {
         }
     }
 
+    let mut stats = (0, 0);
+
     // 5s ぶんまわす
     while start_time.elapsed() < Duration::from_millis(5000) {
-        // do something
+        stats.0 += 1;
+        let idx1 = rng.gen_range(0, block1.len());
+        let idx2 = rng.gen_range(0, block2.len());
+        if block1[idx1].len() == 0 {
+            continue;
+        }
+        if block2[idx2].len() == 0 {
+            continue;
+        }
+        if !is_same(&block1[idx1], &block2[idx2]) {
+            continue;
+        }
+        let mut new_block1 = block1.clone();
+        let mut new_ans1 = ans1.clone();
+
+        let mut changed = false;
+        for &(x, y, z) in &block1[idx1] {
+            for d in 0..6usize {
+                let nx = (x as i32 + DX[d]) as usize;
+                let ny = (y as i32 + DY[d]) as usize;
+                let nz = (z as i32 + DZ[d]) as usize;
+                // 範囲外
+                if nx >= d || ny >= d || nz >= d {
+                    continue;
+                }
+                // すでに同じブロック
+                if new_ans1[nx][ny][nz] == new_ans1[x][y][z] {
+                    continue;
+                }
+                // 実装がめんどいのでとりあえず1つだけをmerge
+                if new_ans1[nx][ny][nz] > 0 {
+                    let oldidx = new_ans1[nx][ny][nz] - 1;
+                    if new_block1[oldidx].len() != 1 {
+                        continue;
+                    }
+                    new_block1[idx1].push((nx, ny, nz));
+                    new_block1[oldidx].clear();
+                    new_ans1[nx][ny][nz] = idx1 + 1;
+                    changed = true;
+                    break;
+                }
+            }
+            if changed {
+                break;
+            }
+        }
+
+        let mut new_block2 = block2.clone();
+        let mut new_ans2 = ans2.clone();
+        let mut changed = false;
+        for &(x, y, z) in &block2[idx2] {
+            for d in 0..6usize {
+                let nx = (x as i32 + DX[d]) as usize;
+                let ny = (y as i32 + DY[d]) as usize;
+                let nz = (z as i32 + DZ[d]) as usize;
+                // 範囲外
+                if nx >= d || ny >= d || nz >= d {
+                    continue;
+                }
+                // すでに同じブロック
+                if new_ans2[nx][ny][nz] == new_ans2[x][y][z] {
+                    continue;
+                }
+                // 実装がめんどいのでとりあえず1つだけをmerge
+                if new_ans2[nx][ny][nz] > 0 {
+                    let oldidx = new_ans2[nx][ny][nz] - 1;
+                    if new_block2[oldidx].len() != 1 {
+                        continue;
+                    }
+                    new_block2[idx2].push((nx, ny, nz));
+                    new_block2[oldidx].clear();
+                    if is_same(&new_block1[idx1], &new_block2[idx2]) {
+                        changed = true;
+                        break;
+                    } else {
+                        // revert
+                        new_block2[idx2].pop();
+                        new_block2[oldidx].push((nx, ny, nz));
+                    }
+                }
+            }
+            if changed {
+                break;
+            }
+        }
+
+        if changed {
+            mem::swap(&mut block1, &mut new_block1);
+            mem::swap(&mut ans1, &mut new_ans1);
+            mem::swap(&mut block2, &mut new_block2);
+            mem::swap(&mut ans2, &mut new_ans2);
+            stats.1 += 1;
+        }
     }
 
     // ブロックIDの修正
     block1.retain(|b| b.len() > 0);
     block2.retain(|b| b.len() > 0);
+    block1.sort_by_key(|b| b.len());
+    block2.sort_by_key(|b| b.len());
+    let mut mp = HashMap::<usize, usize>::new();
+    let mut used = HashSet::<usize>::new();
+    for (i2, b2) in block2.iter().enumerate() {
+        for (i1, b1) in block1.iter().enumerate() {
+            if used.contains(&i1) {
+                continue;
+            }
+            if is_same(b1, b2) {
+                mp.insert(i2, i1);
+                used.insert(i1);
+                break;
+            }
+        }
+    }
+
     for (i, b) in block1.iter().enumerate() {
         for &(x, y, z) in b {
             ans1[x][y][z] = i + 1;
         }
     }
+    let mut nullcnt = 0;
     for (i, b) in block2.iter().enumerate() {
         for &(x, y, z) in b {
-            ans2[x][y][z] = i + 1;
+            if mp.contains_key(&i) {
+                ans2[x][y][z] = mp.get(&i).unwrap() + 1;
+            } else {
+                nullcnt += 1;
+                ans2[x][y][z] = block1.len() + nullcnt;
+            }
         }
     }
 
     // 出力
+    eprintln!("Stats: attempt={}, success={}", stats.0, stats.1);
     output(ans1, ans2);
 }
